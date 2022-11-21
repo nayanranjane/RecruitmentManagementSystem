2e7798c655a6d0937c53c24b21fb063a76847f52
Nayan Ranjane <nayan.ranjane@coditas.com>
2022-11-21 18:06:02 +0530

    UserModuleCreated

---
+++ RecruitmentAdministrationSystemProject/Controllers/CandidateController.cs
@@ -0,0 +1,27 @@
+﻿using RecruitmentAdministrationSystemProject.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RecruitmentAdministrationSystemProject.Controllers
+{
+    public class CandidateController : Controller
+    {
+        RecruitmentManagementSystemEntities dbAccess = new RecruitmentManagementSystemEntities();
+        public ActionResult Index()
+        {
+            var candidates = dbAccess.CandidateInfoes.ToList();
+            return View(candidates);
+        }
+        public ActionResult CandidateDetails(int? id)
+        {
+            var candidates = dbAccess.CandidateInfoes.ToList().Where(candidateUser => candidateUser.UserId == id).ToList();
+            var Candidate = new CandidateInfo();
+            Candidate = candidates.First();
+            return View(Candidate);
+
+        }
+    }
+}
---
+++ RecruitmentAdministrationSystemProject/Controllers/UserController.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using RecruitmentAdministrationSystemProject.Models;
+
+namespace RecruitmentAdministrationSystemProject.Controllers
+{
+    public class UserController : Controller
+    {
+        // GET: User
+        RecruitmentManagementSystemEntities dbAccess = new RecruitmentManagementSystemEntities();
+        public ActionResult Index()
+        {
+            var Users = dbAccess.Users.ToList();
+            return View(Users);
+
+        }
+        public ActionResult Create()
+        {
+            User user = new User();
+            var roles = dbAccess.Roles.ToList().Skip(1).ToList();
+            ViewBag.roles = roles;
+            return View(user);
+        }
+        [HttpPost]
+        public ActionResult Create(User user)
+        {
+            var result = dbAccess.Users.Add(user);
+            dbAccess.SaveChanges();
+            dbAccess.SaveChanges();
+            switch (user.RoleId)
+            {
+                case 2:
+                    return RedirectToAction("CreateCandidateInfo", new { id = user.UserId });
+                    break;
+                case 3:
+                    return RedirectToAction("CreateCompanyInformation", new { id = user.UserId });
+                    break;
+                case 4:
+                    return RedirectToAction("CreateStaffInformation", new { id = user.UserId });
+                    break;
+            }
+            return RedirectToAction("Index");
+
+        }
+
+        public ActionResult CreateCandidateInfo(int id)
+        {
+            CandidateInfo info = new CandidateInfo() { UserId = id };
+            return View(info);
+        }
+        [HttpPost]
+        public ActionResult CreateCandidateInfo(CandidateInfo info)
+        {
+            var result = dbAccess.CandidateInfoes.Add(info);
+            dbAccess.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public ActionResult CreateCompanyInformation(int id)
+        {
+            Company info = new Company() { UserId = id };
+            return View(info);
+        }
+        [HttpPost]
+        public ActionResult CreateCompanyInformation(Company info)
+        {
+            var result = dbAccess.Companies.Add(info);
+            dbAccess.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public ActionResult CreateStaffInformation(int id)
+        {
+            Staff info = new Staff() { UserID = id };
+            var companies = dbAccess.Companies.ToList();
+            ViewBag.companies = companies;
+            return View(info);
+        }
+        [HttpPost]
+        public ActionResult CreateStaffInformation(Company info)
+        {
+            var result = dbAccess.Companies.Add(info);
+            dbAccess.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public ActionResult DeleteUser(int? id)
+        {
+            var result = dbAccess.Users.Find(id);
+            var isRemoved = dbAccess.Users.Remove(result);
+            dbAccess.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public ActionResult DetailsUser(int? id)
+        {
+            User result = new User();
+            result = dbAccess.Users.Find(id);
+            return View(result);
+        }
+        public ActionResult CandidateDetails(int? id)
+        {
+            return RedirectToAction("CandidateDetails", "Candidate", new {id=id});
+
+        }
+
+    }
+}
